5c36a2efeb5619ae068e1284cc13cd258cae8e9a
sarabsingh24 <sarabpreet33@gmail.com>
2023-03-23 09:10:44 +0530

    Updates

---
+++ precache-manifest.27a10b58a28c6bd4ade9666ab27e8f03.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23f6896e0241b709ea4e2c78a067bea8",
+    "url": "/react-work/index.html"
+  },
+  {
+    "revision": "988a1b8b7d1b1f81ffca",
+    "url": "/react-work/static/css/main.b6097767.chunk.css"
+  },
+  {
+    "revision": "f007e6d015ada9488920",
+    "url": "/react-work/static/js/2.e62c44c5.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-work/static/js/2.e62c44c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "988a1b8b7d1b1f81ffca",
+    "url": "/react-work/static/js/main.f3a21a94.chunk.js"
+  },
+  {
+    "revision": "ac30247450673fa1d7e4",
+    "url": "/react-work/static/js/runtime-main.72acb93a.js"
+  },
+  {
+    "revision": "8b929915439a8d1ce388da6f91488118",
+    "url": "/react-work/static/media/item-1.8b929915.jpeg"
+  },
+  {
+    "revision": "9900a731aa92beb3604918ec1ad11526",
+    "url": "/react-work/static/media/item-2.9900a731.jpeg"
+  },
+  {
+    "revision": "f6fffc793ba9ca89c0a8a50ce6960a66",
+    "url": "/react-work/static/media/item-3.f6fffc79.jpeg"
+  },
+  {
+    "revision": "fdf5de3d48fd9563036ce3a5e83fa779",
+    "url": "/react-work/static/media/item-4.fdf5de3d.jpeg"
+  },
+  {
+    "revision": "68ad10f1a59f00591db8d80831c67e59",
+    "url": "/react-work/static/media/item-5.68ad10f1.jpeg"
+  },
+  {
+    "revision": "f62370a35f8f32530478f42e73a60f60",
+    "url": "/react-work/static/media/item-6.f62370a3.jpeg"
+  },
+  {
+    "revision": "6a9cad582f29f822bf661c221a9cebb7",
+    "url": "/react-work/static/media/item-7.6a9cad58.jpeg"
+  },
+  {
+    "revision": "28b2ec1c05cd81f3da9a6bc21bb688e0",
+    "url": "/react-work/static/media/item-8.28b2ec1c.jpeg"
+  },
+  {
+    "revision": "38b6a11ed25d1ad54b95dd763555834f",
+    "url": "/react-work/static/media/item-9.38b6a11e.jpeg"
+  },
+  {
+    "revision": "1361163ab683e185ff5293533a64a1d3",
+    "url": "/react-work/static/media/logo.1361163a.png"
+  },
+  {
+    "revision": "3fb5b40dd905f57d95b4a0098ec089b8",
+    "url": "/react-work/static/media/thailand-travel.3fb5b40d.jpg"
+  }
+]);
